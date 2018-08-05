78a57827daf2ec7078a7021de2331deced6f2e5a
Yury Belousov <yury-belousov@yandex.ru>
2018-08-05 16:09:20 +0300

    add db class

---
+++ app/src/main/java/com/example/beardie/currencyholder/data/local/db/CurrencyDatabase.kt
@@ -0,0 +1,63 @@
+package com.example.beardie.currencyholder.data.local.db
+
+import android.arch.persistence.db.SupportSQLiteDatabase
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.example.beardie.currencyholder.data.local.dao.BalanceDao
+import com.example.beardie.currencyholder.data.local.dao.CategoryDao
+import com.example.beardie.currencyholder.data.local.dao.TransactionDao
+import com.example.beardie.currencyholder.data.local.entity.Balance
+import com.example.beardie.currencyholder.data.local.entity.Category
+import com.example.beardie.currencyholder.data.local.entity.Transaction
+import java.util.concurrent.Executors
+
+private const val DATABASE_NAME = "currency-db"
+
+@Database(entities =
+    [Balance::class,
+    Category::class,
+    Transaction::class], version = 1,
+        exportSchema = false)
+abstract class CurrencyDatabase : RoomDatabase() {
+    abstract fun balanceDao(): BalanceDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun transactionDao(): TransactionDao
+
+    // how to build db
+    // https://github.com/googlesamples/android-sunflower/blob/master/app/src/main/java/com/google/samples/apps/sunflower/data/AppDatabase.kt#L39
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile private var instance : CurrencyDatabase? = null
+
+
+        fun getInstance(context: Context) : CurrencyDatabase {
+            return instance ?:  synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        // Create and pre-populate the database. See this article for more details:
+        // https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1#4785
+        private fun buildDatabase(context: Context) : CurrencyDatabase {
+            return Room.databaseBuilder(context, CurrencyDatabase::class.java, DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            // TODO: move to workManager
+                            Executors.newSingleThreadScheduledExecutor().execute {
+                                // spread operator
+                                // https://kotlinlang.org/docs/reference/functions.html#variable-number-of-arguments-varargs
+                                getInstance(context).balanceDao().insertAll(*SeedDatabase.balances)
+                                getInstance(context).categoryDao().insertAll(*SeedDatabase.category)
+                                getInstance(context).transactionDao().insertAll(*SeedDatabase.transactions)
+                            }
+                        }
+                    })
+                    .build()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/beardie/currencyholder/data/local/db/SeedDatabase.kt
@@ -0,0 +1,51 @@
+package com.example.beardie.currencyholder.data.local.db
+
+import android.graphics.Color
+import com.example.beardie.currencyholder.data.enum.TypeCategoryEnum
+import com.example.beardie.currencyholder.data.local.entity.Balance
+import com.example.beardie.currencyholder.data.model.FinanceCurrency
+import com.example.beardie.currencyholder.data.local.entity.Transaction
+import com.example.beardie.currencyholder.data.local.entity.Category
+import com.example.beardie.currencyholder.data.model.*
+import java.util.Date
+
+object SeedDatabase {
+
+    val currency = listOf(
+            FinanceCurrency(1, "₽", "Rubles", "RUB", ""),
+            FinanceCurrency(2, "$", "Dollars", "USD", "")
+    )
+
+    var category = arrayOf(
+            Category(1, "Зарплата", TypeCategoryEnum.INCOME, Color.GREEN),
+            Category(2, "Стипендия", TypeCategoryEnum.INCOME, Color.GREEN),
+            Category(3, "Коммунальные платежи", TypeCategoryEnum.OUTGO, Color.RED),
+            Category(4, "Одежда", TypeCategoryEnum.OUTGO, Color.RED),
+            Category(5, "Питание", TypeCategoryEnum.OUTGO, Color.RED),
+            Category(6, "Развлечения", TypeCategoryEnum.OUTGO, Color.RED),
+            Category(7, "Другое", TypeCategoryEnum.OUTGO, Color.RED)
+    )
+
+    val exchange = listOf(
+            ExchangeCurrency(1, currency[0], currency[1], 0.016),
+            ExchangeCurrency(2, currency[1], currency[0], 63.49)
+    )
+
+    var balances = arrayOf(
+            Balance(1, "Наличные", -5000.0, currency[0]),
+            Balance(2, "Карта", 100.0, currency[1])
+    )
+
+    var transactions = arrayOf(
+            Transaction(1, 1000.0, balances[1], currency[0], Date(), category[0]),
+            //Transaction(2,  50000.0, balances[0], currency[1], category[0]),
+            Transaction(3, 99.0, balances[1], currency[0], Date(), category[0]),
+            Transaction(4, 100.0, balances[1], currency[0], Date(), category[0]),
+            Transaction(5, -90.0, balances[1], currency[0], Date(), category[3]),
+            Transaction(6, -99.0, balances[1], currency[0], Date(), category[4]),
+            Transaction(7, -100.0, balances[1], currency[0], Date(), category[4]),
+            Transaction(8, -90.0, balances[1], currency[0], Date(), category[5]),
+            Transaction(9, 99.0, balances[1], currency[0], Date(), category[0]),
+            Transaction(10, -133.0, balances[1], currency[0], Date(), category[5])
+    )
+}
