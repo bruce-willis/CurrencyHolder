a84422fbdf7ff5cb698cc3c6c622a53de796eb6d
Yury Belousov <yury-belousov@yandex.ru>
2018-08-07 23:41:30 +0300

    make converter NonNullable

--- app/src/main/java/com/example/beardie/currencyholder/data/local/converter/DateConverter.kt
+++ app/src/main/java/com/example/beardie/currencyholder/data/local/converter/DateConverter.kt
@@ -5,12 +5,12 @@ import java.util.*
 
 class DateConverter {
     @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return if (value == null) null else Date(value)
+    fun fromTimestamp(value: Long): Date? {
+        return Date(value)
     }
 
     @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time
+    fun dateToTimestamp(date: Date): Long? {
+        return date.time
     }
 }
